b005dcc5b616352595a4c8781f32f54db8243dbd
Maaz Patel <maazptl240602@gmail.com>
2024-05-22 17:25:55 +0530

    Infra/providers.tf updated

--- Infra/providers.tf
+++ Infra/providers.tf
@@ -5,12 +5,12 @@ terraform {
       version = "3.46.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "sa1_test_eic_MaazPatel"
-    storage_account_name = "terraformremotestate24"
-    container_name       = "azure-devops-pipeline"
-    key                  = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "sa1_test_eic_MaazPatel"
+  #   storage_account_name = "terraformremotestate24"
+  #   container_name       = "azure-devops-pipeline"
+  #   key                  = "terraform.tfstate"
+  # }
 }
 
 provider "azurerm" {
