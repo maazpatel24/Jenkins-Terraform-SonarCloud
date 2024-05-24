3551adebde26a2e8dd5a6190158bc297ba3445ba
Maaz Patel <maazptl240602@gmail.com>
2024-05-24 11:47:49 +0530

    Infra/ changesss

--- Infra/output.tf
+++ Infra/output.tf
@@ -1,7 +1,7 @@
-# output "rg_name" {
-#   value       = data.azurerm_resource_group.maaz_rg.id
-#   description = "Record the value of resourse-group name, given by the data source"
-# }
+output "rg_name" {
+  value       = data.azurerm_resource_group.maaz_rg.id
+  description = "Record the value of resourse-group name, given by the data source"
+}
 
 output "public_ip_address" {
   value = azurerm_linux_virtual_machine.example_linux_vm.public_ip_address
